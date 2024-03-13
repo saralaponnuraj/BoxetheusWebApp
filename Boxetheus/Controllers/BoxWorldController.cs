c16e806c8432248208716e95663c7bec3ffe4b56
Sarala Ponnuraj <sp384@myscc.ca>
2024-03-13 18:45:25 -0400

    Add project files.

---
+++ Boxetheus/Controllers/BoxWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Boxetheus.Controllers
+{
+    public class BoxWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
