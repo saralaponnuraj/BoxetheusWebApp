26a33f91f067b62e9aa0f9e3c48da8528b95646b
Eshikalad <158506227+Eshikalad@users.noreply.github.com>
2024-03-15 21:39:40 -0400

    Update Program.cs

--- Boxetheus/Program.cs
+++ Boxetheus/Program.cs
@@ -13,7 +13,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+//Add Roles to Login
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
